a32942564abcd8794fb0a5f4915c09f3045f8a99
knokko <knokogator@hotmail.com>
2020-01-10 19:21:44 +0100

    Write test cases for the constructor, close() and closeExact() methods of VirtualStack

--- vulko-memory/src/test/kotlin/vulko/memory/TestVirtualStack.kt
+++ vulko-memory/src/test/kotlin/vulko/memory/TestVirtualStack.kt
@@ -1,7 +1,9 @@
 package vulko.memory
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Test
+import vulko.memory.util.free
+import vulko.memory.util.malloc
 
 class TestVirtualStack {
 
@@ -38,5 +40,55 @@ class TestVirtualStack {
         // Bad cases
         testBadConstructor(-34934, -3847485)
         testBadConstructor(348734, -1234)
+    }
+
+    @Test
+    fun testClose() {
+        val splitter = TestSplitter()
+        val stack = VirtualStack(splitter, 123, 100)
+        assertFalse(stack.isClosed())
+        assertEquals(null, splitter.lastFreedAddress)
+        stack.close()
+        assertTrue(stack.isClosed())
+        assertEquals(123L as Long?, splitter.lastFreedAddress)
+        splitter.lastFreedAddress = null
+        try {
+            stack.close()
+            throw AssertionError("Closing a VirtualStack twice should throw" +
+                    "an IllegalStateException")
+        } catch (ex: IllegalStateException) {
+            assertEquals(null as Long?, splitter.lastFreedAddress)
+        }
+    }
+
+    @Test
+    fun testCloseExact() {
+
+        // Set-up
+        val splitter = TestSplitter()
+        val capacity = 4L
+        val address = malloc(capacity)
+        val stack = VirtualStack(splitter, address, capacity)
+
+        // The actual test
+        stack.pushInt(12)
+        try {
+            stack.closeExact()
+            throw AssertionError("VirtualStack.closeExact should have thrown" +
+                    "an IllegalStateException")
+        } catch (ex: IllegalStateException) {
+
+            // The close should have been aborted before any action was taken
+            assertEquals(null, splitter.lastFreedAddress)
+            assertFalse(stack.isClosed())
+        }
+
+        stack.popInt()
+        stack.closeExact()
+        assertTrue(stack.isClosed())
+        assertEquals(address as Long?, splitter.lastFreedAddress)
+
+        // Clean-up
+        free(address)
     }
 }
